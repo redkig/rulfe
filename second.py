4a371f9b40742350782edaebe8440d9c9ca95c91
redkig <132080259+redkig@users.noreply.github.com>
2023-06-19 13:43:20 +0600

    Add files via upload

---
+++ second.py
@@ -0,0 +1,24 @@
+#2 экран
+import kivy
+from kivy.app import App
+from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
+from kivy.clock import Clock
+class Seconds(Label):
+   
+    def __init__(self, total, **kwargs):
+        pass
+
+    def restart(self, total, **kwargs):
+        pass
+
+    def start(self):
+        pass
+
+    def change(self, dt):
+        pass
